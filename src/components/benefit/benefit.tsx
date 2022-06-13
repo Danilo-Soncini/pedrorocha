905dfdf2c4763ce0298aead4395d5764379c6b11
luccasoncini <luccadiassoncini@gmail.com>
2022-06-13 15:50:03 -0300

    refazendo a parte de vantagens

--- src/components/benefit/benefit.tsx
+++ src/components/benefit/benefit.tsx
@@ -1,105 +1,63 @@
+import { useState } from "react";
 import { TiInputChecked } from "react-icons/ti";
 import styles from './benefit.module.scss'
 
+interface benefitInterface {
+    benefit: string
+}
 
 export function Benefit() {
+
+    const [benefits, useBenefits] = useState<benefitInterface[]>([
+        {
+            benefit: 'Treine de onde você quiser!'
+        },
+        {
+            benefit: 'Treinos online e ao vivo!'
+        },
+        {
+            benefit: 'Lives salvas para sempre para assinantes ativos!'
+        },
+        {
+            benefit: 'Treinos com e sem equipamentos!'
+        },
+        {
+            benefit: 'Perfil exclusivo para assinantes!'
+        },
+        {
+            benefit: 'Acompanhamento do progresso dos alunos!'
+        },
+        {
+            benefit: 'Acesso a treinos exclusivos!'
+        },
+        {
+            benefit: 'Possibilidade de tirar dúvidas com o professor durante as lives!'
+        },
+        
+    ])
+
     return(
         <section className={styles.benefit_section}>
             <div className="container">
-            <div className="row">
-                <div className="col-12">
-                <div className={styles.benefit_major_content}>
-                    <h3>Vantagens de adquirir um plano com Pedro Rocha</h3>
-                    <div className={styles.benefit_cards}>
-                    <ul>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Treine de onde você quiser!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Treinos online e ao vivo!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Lives salvas para sempre para assinantes ativos!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Treinos de peso corporal!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Perfil exclusivo para assinantes!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>3 treinos ao vivo por dia!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Acesso a 8 links de treinos ao vivo e exclusivos!</span>
-                        </li>
-                    </ul>
-                    <div className={styles.cards_prices}>
-                        <small>Por apenas</small>
-                        <div className={styles.prices_container}>
-                            <div className={styles.price}>
-                            <span>
-                                R$ 39,99
-                            </span>
+                <div className="row">
+                    <div className="col-12">
+                        <div className={styles.benefit_major_content}>
+                            <small>Ficou interessado?</small>
+                            <h3>Vantagens do perfil privado</h3>
+                            <div className={styles.benefit_cards}>
+                                <ul>
+                                    {benefits.map(({ benefit }, i) => (
+                                        <li>
+                                        <TiInputChecked/>
+                                        <span>{benefit}</span>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
+                            <a href="#form">Clique e treine comigo!</a>
                         </div>
-                        <small>por mês</small>
-                    </div>
-                    </div>
-                    <div className={styles.benefit_cards}>
-                    <ul>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Treine de onde você quiser!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Treinos online e ao vivo!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Lives salvas para sempre para assinantes ativos!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Treinos de peso corporal!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Perfil exclusivo para assinantes!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>3 treinos ao vivo por dia!</span>
-                        </li>
-                        <li>
-                        <TiInputChecked/>
-                        <span>Acesso a 12 links de treinos ao vivo e exclusivos!</span>
-                        </li>
-                    </ul>
-                    <div className={styles.cards_prices}>
-                        <small>Por apenas</small>
-                        <div className={styles.prices_container}>
-                            <div className={styles.price}>
-                            <span>
-                                R$ 49,99
-                            </span>
-                            </div>
-                        </div>
-                        <small>por mês</small>
-                    </div>
-                    </div>
-                    <a href="#form">Clique e treine comigo!</a>
+                    </div> 
                 </div>
-                </div> 
-            </div>
             </div>
         </section>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.scss'
 import { Benefit } from '../components/benefit/benefit'
+// import { Benefit2} from '../components/benefit2/benefit2'
 import { Faq } from '../components/faq/faq'
 import { Feedbacks } from '../components/feedbacks/feedbacks'
 import { ContactForm } from '../components/form/form'
@@ -24,13 +25,19 @@ function Home() {
 
         <Benefit />
 
-        <ContactForm />
-
-        <Whatsapp />
-
+        {/* <Benefit2 /> */}
+        
         <Feedbacks />
+
+        <ContactForm />
         
         <Faq />
+
+
+
+
+
+        <Whatsapp />
 
       </main>
     </>
