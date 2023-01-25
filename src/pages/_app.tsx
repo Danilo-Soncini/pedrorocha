1b76c47163092feda71dee793faf59487d646cff
LuccaModerniza <lucca@moderniza.group>
2023-01-25 08:56:18 -0300

    add pixel faceboook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
 
-import '../../styles/global.scss'
-import "swiper/css/bundle";
-
+import "../../styles/global.scss"
+import "swiper/css/bundle"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("1050387735671240") // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
